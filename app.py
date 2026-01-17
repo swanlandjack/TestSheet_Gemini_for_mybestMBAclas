6b3199670fcb017617d68a6e67b00fa1ac1f379c
swanlandjack <37183621+swanlandjack@users.noreply.github.com>
2026-01-17 12:22:54 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -110,4 +110,5 @@ def data_info():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
